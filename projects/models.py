f6776b95268ece844d305b893ec4534e0ea245cd
Sean Carley <sean.carley@turasmobility.com>
2022-06-15 08:30:32 +0100

    added Project model in Project app

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,22 @@
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    ''' model holding project information '''
+
+    class Meta:
+        ''' change the table name in admin page '''
+        verbose_name_plural = 'Project_Details'
+
+proj_name = models.CharField(max_length=75, null=False, blank=False)
+proj_type = models.CharField(max_length=50, null=False, blank=False)
+proj_description = models.TextField(null=False, blank=False, default='')
+proj_link = models.CharField(max_length=200, null=False, blank=False)
+user_id = models.ForeignKey(User, to_field='id', on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.proj_name
+
